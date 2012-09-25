3fe7458b19a0f28a603fff26a5fbd79f073d439f
Paul Picazo <ppicazo@gmail.com>
2012-09-25 00:16:43 -0700

    Remove '-newNameID4' option when building TTF.

--- build.sh
+++ build.sh
@@ -10,6 +10,6 @@ mkdir target/
 for w in ${weights[@]};
 do
   makeotf -sp target/$family-$w-otf.fpr -f Roman/$w/font.pfa -r -o target/$family-$w.otf
-  makeotf -sp target/$family-$w-ttf.fpr -f Roman/$w/font.ttf -gf GlyphOrderAndAliasDB_TT -newNameID4 -r -o target/$family-$w.ttf
+  makeotf -sp target/$family-$w-ttf.fpr -f Roman/$w/font.ttf -gf GlyphOrderAndAliasDB_TT -r -o target/$family-$w.ttf
   rm Roman/$w/current.fpr # remove default options file from the source tree after building
 done
