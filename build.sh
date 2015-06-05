fa7cfcd8fef1f22c0022d64473486d2dddae0f90
Miguel Sousa <msousa@adobe.com>
2015-06-04 17:59:51 -0700

    updates build script

--- build.sh
+++ build.sh
@@ -1,15 +1,23 @@
 #!/bin/sh
 
 family=SourceCodePro
-weights='Black Bold ExtraLight Light Medium Regular Semibold'
+romanWeights='Black Bold ExtraLight Light Medium Regular Semibold'
+italicWeights='BlackIt BoldIt ExtraLightIt LightIt MediumIt It SemiboldIt'
 
 # clean existing build artifacts
 rm -rf target/
 mkdir target/ target/OTF/ target/TTF/
 
-for w in $weights
+for w in $romanWeights
 do
-  makeotf -f Roman/$w/font.pfa -r -o target/OTF/$family-$w.otf
-  makeotf -f Roman/$w/font.ttf -gf GlyphOrderAndAliasDB_TT -r -o target/TTF/$family-$w.ttf
+  makeotf -f Roman/$w/font.ufo -r -o target/OTF/$family-$w.otf
+  makeotf -f Roman/$w/font.ttf -r -o target/TTF/$family-$w.ttf
   rm Roman/$w/current.fpr # remove default options file from the source tree after building
+done
+
+for w in $italicWeights
+do
+  makeotf -f Italic/$w/font.ufo -r -o target/OTF/$family-$w.otf
+  makeotf -f Italic/$w/font.ttf -r -o target/TTF/$family-$w.ttf
+  rm Italic/$w/current.fpr # remove default options file from the source tree after building
 done
