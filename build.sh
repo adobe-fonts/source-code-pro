5312ce785673605ea77acc33ce4f701da09cc66c
Miguel Sousa <msousa@adobe.com>
2015-06-25 02:07:58 -0700

    Adds Python script to inject SVG tables into fonts, and adds SVG step to build script

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@ family=SourceCodePro
 romanWeights='Black Bold ExtraLight Light Medium Regular Semibold'
 italicWeights='BlackIt BoldIt ExtraLightIt LightIt MediumIt It SemiboldIt'
 
+# path to Python script that adds the SVG table
+addSVG=$(cd $(dirname "$0") && pwd -P)/addSVGtable.py
+
 # clean existing build artifacts
 rm -rf target/
 mkdir target/ target/OTF/ target/TTF/
@@ -13,6 +16,8 @@ do
   makeotf -f Roman/$w/font.ufo -r -o target/OTF/$family-$w.otf
   makeotf -f Roman/$w/font.ttf -r -o target/TTF/$family-$w.ttf
   rm Roman/$w/current.fpr # remove default options file from the source tree after building
+  $addSVG target/OTF/$family-$w.otf svg/
+  $addSVG target/TTF/$family-$w.ttf svg/
 done
 
 for w in $italicWeights
@@ -20,4 +25,6 @@ do
   makeotf -f Italic/$w/font.ufo -r -o target/OTF/$family-$w.otf
   makeotf -f Italic/$w/font.ttf -r -o target/TTF/$family-$w.ttf
   rm Italic/$w/current.fpr # remove default options file from the source tree after building
+  $addSVG target/OTF/$family-$w.otf svg/
+  $addSVG target/TTF/$family-$w.ttf svg/
 done
