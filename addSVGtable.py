67b29553521f9d531a79f9c68b497af57ba8b50f
Miguel Sousa <msousa@adobe.com>
2016-05-02 02:14:46 -0700

    Strip white space between SVG elements

--- addSVGtable.py
+++ addSVGtable.py
@@ -32,6 +32,7 @@ TABLE_TAG = 'SVG '
 reSVGelement = re.compile(r"<svg.+?>.+?</svg>", re.DOTALL)
 reIDvalue = re.compile(r"<svg[^>]+?(id=\".*?\").+?>", re.DOTALL)
 reViewBox = re.compile(r"<svg.+?(viewBox=[\"|\'][\d, ]+[\"|\']).+?>", re.DOTALL)
+reWhiteSpace = re.compile(r">\s+<", re.DOTALL)
 
 
 def readFile(filePath):
@@ -84,7 +85,10 @@ def processFontFile(fontFilePath, svgFilePathsList):
 		svgItemData = readFile(svgFilePath)
 		svgItemData = setIDvalue(svgItemData, gid)
 		svgItemData = fixViewBox(svgItemData)
-		svgItemsList.append(svgItemData)
+		# Remove all white space between elements
+		for whiteSpace in set(reWhiteSpace.findall(svgItemData)):
+			svgItemData = svgItemData.replace(whiteSpace, '><')
+		svgItemsList.append(svgItemData.strip())
 		svgItemsList.extend([gid, gid])
 		svgDocsDict[gid] = svgItemsList
 
