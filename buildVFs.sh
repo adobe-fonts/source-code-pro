6ab3c0053c82034c946f906b9ffc74afaf61e85b
Miguel Sousa <msousa@adobe.com>
2017-01-21 22:38:00 -0800

    refactoring of master feature files

--- buildVFs.sh
+++ buildVFs.sh
@@ -1,2 +1,8 @@
 buildMasterOTFs RomanMasters/SourceCodePro.designspace
-buildCFF2VF RomanMasters/SourceCodePro.designspace  SourceCodePro-Variable.otf
+buildCFF2VF RomanMasters/SourceCodePro.designspace  RomanMasters/SourceCodePro-Variable.otf
+
+buildMasterOTFs ItalicMasters/SourceCodePro-It.designspace
+buildCFF2VF ItalicMasters/SourceCodePro-It.designspace  ItalicMasters/SourceCodePro-VariableItalic.otf
+
+rm *Masters/master_*/current.fpr
+rm *Masters/master_*/master.otf
