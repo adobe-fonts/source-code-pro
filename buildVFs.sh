c272913da4c786834f75eab9fe34979e7a0366bb
Miguel Sousa <msousa@adobe.com>
2017-01-22 00:12:48 -0800

    designspace files resaved with DesignSpaceEditor RF extension

--- buildVFs.sh
+++ buildVFs.sh
@@ -1,8 +1,8 @@
 buildMasterOTFs RomanMasters/SourceCodePro.designspace
 buildCFF2VF RomanMasters/SourceCodePro.designspace  RomanMasters/SourceCodePro-Variable.otf
 
-buildMasterOTFs ItalicMasters/SourceCodePro-It.designspace
-buildCFF2VF ItalicMasters/SourceCodePro-It.designspace  ItalicMasters/SourceCodePro-VariableItalic.otf
+buildMasterOTFs ItalicMasters/SourceCodePro-Italic.designspace
+buildCFF2VF ItalicMasters/SourceCodePro-Italic.designspace  ItalicMasters/SourceCodePro-VariableItalic.otf
 
 rm *Masters/master_*/current.fpr
 rm *Masters/master_*/master.otf
